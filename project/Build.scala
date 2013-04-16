4c6611b8b44e00a1228daa63cec28ba6ac0f465b
Kenichiro TANAKA <tanaka.kenichiro@gmail.com>
2013-04-16 10:05:19 +0900

    add bootstrap less compile

--- project/Build.scala
+++ project/Build.scala
@@ -13,9 +13,19 @@ object ApplicationBuild extends Build {
     anorm
   )
 
+  // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+  def customLessEntryPoints(base: File): PathFinder = (
+      (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+      (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap-responsive.less") +++
+      (base / "app" / "assets" / "stylesheets" * "*.less")
+  )
+
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here   
+
+    // twitter bootstrap less setting
+    lessEntryPoints <<= baseDirectory(customLessEntryPoints)   
   )
 
 }
