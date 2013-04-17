f10de208e01b7d24ed131bb708a88c9d61ad4e7a
Kenichiro TANAKA <tanaka.kenichiro@gmail.com>
2013-04-17 19:21:17 +0900

    add README

--- slick/app/models/Users.scala
+++ slick/app/models/Users.scala
@@ -2,15 +2,6 @@ package models
 
 import play.api.db.slick.Config.driver.simple._
 
-
-/**
- * Created with IntelliJ IDEA.
- * User: ktana
- * Date: 13/04/17
- * Time: 9:46
- * To change this template use File | Settings | File Templates.
- */
-
 case class User(id: Long, name: String, email: String)
 
 object Users extends Table[User]("USER") {
